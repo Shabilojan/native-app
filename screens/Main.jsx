c6eca0a74f182fd33f9d841539750cd4597c2019
R.Shabilojan <Shabilojan12@gmail.com>
2024-06-08 09:14:24 +0530

    updated

--- screens/Main.jsx
+++ screens/Main.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, BackHandler } from 'react-native';
+import { Image, View, StyleSheet, BackHandler } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import { launchImageLibrary, launchCamera } from 'react-native-image-picker';
 
@@ -32,8 +32,8 @@ const Main = ({ route, navigation }) => {
     const options = {
       mediaType: 'photo',
       includeBase64: false,
-      maxHeight: 2000,
-      maxWidth: 2000,
+      maxHeight: 600,
+      maxWidth: 340,
     };
 
     launchImageLibrary(options, handleResponse);
@@ -43,8 +43,8 @@ const Main = ({ route, navigation }) => {
     const options = {
       mediaType: 'photo',
       includeBase64: false,
-      maxHeight: 2000,
-      maxWidth: 2000,
+      maxHeight: 600,
+      maxWidth: 340,
     };
 
     launchCamera(options, handleResponse);
@@ -52,9 +52,10 @@ const Main = ({ route, navigation }) => {
 
   const handleResponse = (response) => {
     if (response.didCancel) {
-      console.log('User cancelled image picker');
+      navigation.navigate('Menu');
     } else if (response.error) {
       console.log('Image picker error: ', response.error);
+      navigation.navigate('Menu');
     } else {
       let imageUri = response.uri || response.assets?.[0]?.uri;
       setSelectedImage(imageUri);
@@ -62,16 +63,101 @@ const Main = ({ route, navigation }) => {
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      {selectedImage && (
-        <Image
-          source={{ uri: selectedImage }}
-          style={{ flex: 1 }}
-          resizeMode="contain"
-        />
-      )}
+    <View style={styles.container}>
+      <Image style={styles.backgroundImage} source={require('../assets/BG2.png')} />
+      <View style={styles.overlayContainer}>
+        <View style={styles.cameraContainer}>
+          <View style={styles.cameraPreview}>
+            <View style={styles.cornerTopLeft}></View>
+            <View style={styles.cornerTopRight}></View>
+            <View style={styles.cornerBottomLeft}></View>
+            <View style={styles.cornerBottomRight}></View>
+          </View>
+        </View>
+        {selectedImage && (
+          <Image
+            source={{ uri: selectedImage }}
+            style={styles.selectedImage}
+            resizeMode="contain"
+          />
+        )}
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+  overlayContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cameraContainer: {
+    width: 340,
+    height: 600,
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cameraPreview: {
+    width: 340,
+    height: 600,
+    borderWidth: 2,
+    borderColor: 'gray',
+  },
+  cornerTopLeft: {
+    position: 'absolute',
+    top: -15,
+    left: -15,
+    width: 30,
+    height: 30,
+    borderColor: 'white',
+    borderLeftWidth: 5,
+    borderTopWidth: 5,
+  },
+  cornerTopRight: {
+    position: 'absolute',
+    top: -15,
+    right: -15,
+    width: 30,
+    height: 30,
+    borderColor: 'white',
+    borderRightWidth: 5,
+    borderTopWidth: 5,
+  },
+  cornerBottomLeft: {
+    position: 'absolute',
+    bottom: -15,
+    left: -15,
+    width: 30,
+    height: 30,
+    borderColor: 'white',
+    borderLeftWidth: 5,
+    borderBottomWidth: 5,
+  },
+  cornerBottomRight: {
+    position: 'absolute',
+    bottom: -15,
+    right: -15,
+    width: 30,
+    height: 30,
+    borderColor: 'white',
+    borderRightWidth: 5,
+    borderBottomWidth: 5,
+  },
+  selectedImage: {
+    width: 340,
+    height: 600,
+    position: 'absolute',
+  },
+});
 
 export default Main;
